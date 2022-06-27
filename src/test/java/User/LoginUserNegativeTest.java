e658cd9803d5ae023716648d289361819259ae33
Andrey Shashev <Activated2006@yandex.ru>
2022-06-27 18:51:16 +0300

    Second try

--- src/test/java/User/LoginUserNegativeTest.java
+++ src/test/java/User/LoginUserNegativeTest.java
@@ -4,25 +4,41 @@ import Users.RestAssuredUser;
 import Users.UserClient;
 import Users.UsersCredentials;
 import Users.UsersData;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
+import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class LoginUserTest extends RestAssuredUser {
+public class LoginUserNegativeTest extends RestAssuredUser {
     private UsersData usersData;
     private UserClient userClient;
+    private String token;
 
     @Before
     public void setUp() {
         userClient = new UserClient();
+        usersData = UsersData.getRandom();
+        ValidatableResponse response = userClient.createUser(usersData);
+        token = response
+                .extract()
+                .path("accessToken");
     }
 
+    @After
+    public void tearDown() {
+        userClient.deleteUser(token.substring(7));
+    }
 
     @Test
+    @DisplayName("Login User With Incorrect Password")
+    @Description("Логин клиента в системе с неправильным паролем")
     public void loginUserWithIncorrectPassword() {
-        usersData = new UsersData("call@soul.ru","123");
+        usersData.setPassword(usersData.getPassword()+1);
         UsersCredentials credentials = UsersCredentials.from(usersData);
         userClient.loginUser(credentials)
                 .assertThat()
@@ -32,8 +48,25 @@ public class LoginUserTest extends RestAssuredUser {
     }
 
     @Test
+    @DisplayName("Login User With Incorrect Email")
+    @Description("Логин клиента в системе с неправильным email")
     public void loginUserWithIncorrectEmail() {
-        usersData = new UsersData("all@soul.com","123123");
+        usersData.setEmail(usersData.getEmail()+2);
+        UsersCredentials credentials = UsersCredentials.from(usersData);
+        userClient.loginUser(credentials)
+                .assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .and()
+                .body(equalTo(BODY_401));
+    }
+
+    @Test
+    @DisplayName("Login User With Empty Body")
+    @Description("Логин клиента в системе с пустым телом запроса")
+    public void loginUserWithEmptyBody() {
+        usersData.setEmail(null);
+        usersData.setPassword(null);
+        usersData.setName(null);
         UsersCredentials credentials = UsersCredentials.from(usersData);
         userClient.loginUser(credentials)
                 .assertThat()
